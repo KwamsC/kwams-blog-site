39f7023d6bd1db6e125924d4092d8575126df9c2
Kwame Chan Jong Chu <kwamsc91@gmail.com>
2021-06-29 15:36:51 +0200

    added css folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 const { DateTime } = require('luxon');
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('/src/style.css');
-    eleventyConfig.addPassthroughCopy('/src/assets')
+    eleventyConfig.addPassthroughCopy('/src/css');
+    eleventyConfig.addPassthroughCopy('/src/assets');
     eleventyConfig.addFilter("postDate", (dateObj) =>{
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_MED)
     })
